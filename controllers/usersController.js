d820a8bdd8e45ec156081da5ac3b220aa6fc48f5
CarlBedrot <ca1171be-s@student.lu.se>
2023-12-10 21:04:54 +0100

    Add validation for name and spirit type, and handle errors in usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,6 +9,12 @@ exports.getAllUsers = (req, res) => {
 };
 
 exports.addUser = (req, res) => {
+  const { name } = req.body;
+
+  if (!name || typeof name !== 'string' || name.trim() === '') {
+    return res.status(400).json({ message: "Invalid name." });
+  }
+
   const newUser = new User({ ...req.body });
 
   newUser.save()
@@ -16,17 +22,34 @@ exports.addUser = (req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 };
 
+
 exports.updateSpirit = (req, res) => {
   User.findById(req.body.id)
     .then(user => {
-      user.spirits[req.body.spiritType] += Number(req.body.amount);
+      if (!user) {
+        return res.status(404).json({ message: "User not found." });
+      }
 
+      const validSpiritTypes = Object.keys(user.spirits);
+      const { spiritType, amount } = req.body;
+
+      if (!validSpiritTypes.includes(spiritType)) {
+        return res.status(400).json({ message: "Invalid spirit type." });
+      }
+
+      if (!Number.isFinite(amount) || amount < 0) {
+        return res.status(400).json({ message: "Invalid amount." });
+      }
+
+      user.spirits[spiritType] += Number(amount);
       user.save()
         .then(() => res.json('User updated!'))
         .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
 };
+
+
 
 exports.deleteUser = (req, res) => {
     User.findByIdAndDelete(req.params.id)
